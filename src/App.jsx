5dd3a7a82b0633899f7a47a2aa736b87967456e8
Ilinsanity <Issaclin2002@gmail.com>
2025-01-20 15:47:32 -0500

    formatting

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import "./App.css";
 
 function App() {
   return (
-    <div>
+    <div className=" bg-linen">
       <nav className="bg-navbar p-4 fixed top-0 left-0 w-full z-10 ">
         <div className="max-w-6xl mx-auto flex justify-between items-center">
           <a href="#" className="text-caramel text-3xl font-bold font-abz">
@@ -31,8 +31,8 @@ function App() {
         </div>
       </nav>
 
-      <div className="w-screen h-screen bg-linen flex items-center">
-        <div className="flex flex-col  absolute left-28">
+      <div className="w-4/5 h-screen flex justify-center items-center mx-auto">
+        <div className="flex flex-col">
           <p className="text-caramel text-2xl font-mono font-thin">
             Nice to Meet You, I'm
           </p>
@@ -42,6 +42,13 @@ function App() {
           <p className="text-caramel text-11xl font-jomhuria leading-[0.8] m-0">
             Lin
           </p>
+        </div>
+        <div className="flex flex-col">
+          <img
+            src="/src/images/profile.jpg"
+            alt="Description"
+            className="w-80 h-80 object-contain rounded-lg"
+          />
         </div>
       </div>
     </div>
