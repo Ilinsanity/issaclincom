7f0a6b6f1803489b0698047f4529d036b92905f2
Ilinsanity <Issaclin2002@gmail.com>
2025-04-09 12:54:10 -0400

    added gsap

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,64 @@
+import SplitType from "split-type";
 import "./App.css";
 
+import gsap from "gsap";
+
 function App() {
+  gsap.from(".fname", {
+    x: -120,
+    opacity: 0,
+    duration: 2,
+    ease: "power2.out",
+    delay: 0.5,
+  });
+  gsap.from(".lname", {
+    x: -120,
+    opacity: 0,
+    duration: 2,
+    ease: "power2.out",
+    delay: 0.7,
+  });
+  gsap.from(".pfp", { scale: 0.1, duration: 1, delay: 0.5, opacity: 0 });
+
+  const mytext = new SplitType("#begblu");
+  const mtext = new SplitType("#abtmetext");
+
+  gsap.from("#begblu .char", {
+    y: -100,
+    stagger: 0.05,
+    delay: 0.3,
+    duration: 1,
+    ease: "power4.out",
+  });
+  // gsap.from("#abtmetext .char", {
+  //   y: -300,
+  //   stagger: 0.01,
+  //   delay: 0.2,
+  //   duration: 0.3,
+  //   ease: "power4.out",
+  // });
   return (
     <div className=" bg-linen">
-      {/* <nav className="fixed left-0 top-0 h-full w-20 flex flex-col items-center py-4 space-y-6">
-        <div className="max-w-6xl mx-auto">
-          <a href="#" className="text-caramel text-3xl font-bold font-abz">
-            il
-          </a>
-          <div className="flex space-x-6">
-            <a
-              href="#abtme"
-              className="text-caramel text-base font-bold p-2 rounded-lg hover:bg-black  hover:shadow-lg transition-all duration-300"
-            >
-              about me
-            </a>
-            <a
-              href="#"
-              className="text-caramel text-base font-bold p-2 rounded-lg hover:bg-black  hover:shadow-lg transition-all duration-300"
-            >
-              projects
-            </a>
-            <a
-              href="#"
-              className="text-caramel text-base font-bold p-2 rounded-lg hover:bg-black  hover:shadow-lg transition-all duration-300"
-            >
-              contact
-            </a>
-          </div>
-        </div>
-      </nav> */}
-      <nav className="fixed left-0 top-0 h-full bg-caramel flex flex-col items-center py-4 space-y-16">
-        <a href="#" className="text-linen text-3xl font-bold font-abz">
+      <nav className="fixed left-4 top-4 flex  items-center ">
+        <a href="#" className="text-caramel text-5xl font-bold font-abz ">
           il
         </a>
-        <div className="flex flex-col space-y-4">
+        <div className="flex ml-4">
           <a
             href="#abtme"
-            className="text-linen text-base font-bold p-2 rounded-lg hover:bg-black  hover:shadow-lg transition-all duration-300 text-center"
+            className="text-caramel text-base font-bold p-2 rounded-lg hover:bg-black  hover:shadow-lg transition-all duration-300"
           >
             about me
           </a>
           <a
             href="#projects"
-            className="text-linen text-base font-bold p-2 rounded-lg hover:bg-black  hover:shadow-lg transition-all duration-300 text-center"
+            className="text-caramel text-base font-bold p-2 rounded-lg hover:bg-black  hover:shadow-lg transition-all duration-300"
           >
             projects
           </a>
           <a
             href="#"
-            className="text-linen text-base font-bold p-2 rounded-lg hover:bg-black  hover:shadow-lg transition-all duration-300 text-center"
+            className="text-caramel text-base font-bold p-2 rounded-lg hover:bg-black  hover:shadow-lg transition-all duration-300"
           >
             contact
           </a>
@@ -58,13 +67,13 @@ function App() {
 
       <div className="w-4/5 h-screen flex justify-center items-center mx-auto">
         <div className="flex flex-col">
-          <p className="text-caramel text-2xl font-mono font-thin">
+          <p className="text-caramel text-2xl font-mono font-thin" id="begblu">
             Nice to Meet You, I'm
           </p>
-          <p className="text-caramel text-11xl font-jomhuria leading-[0.6] m-0">
+          <p className="text-caramel text-11xl font-jomhuria leading-[0.6] m-0 fname">
             Issac
           </p>
-          <p className="text-caramel text-11xl font-jomhuria leading-[0.8] m-0">
+          <p className="text-caramel text-11xl font-jomhuria leading-[0.8] m-0 lname">
             Lin
           </p>
         </div>
@@ -72,13 +81,13 @@ function App() {
           <img
             src="/src/images/profile.jpg"
             alt="Description"
-            className="w-80 h-80 object-contain border-8 border-caramel rounded-lg"
+            className="w-80 h-80 object-contain border-8 border-caramel rounded-lg pfp"
           />
         </div>
       </div>
       <div className="w-9/12 h-screen mx-auto text-center" id="abtme">
         <p className="text-caramel text-8xl font-jomhuria py-6">About Me</p>
-        <p className="text-rblack text-3xl font-mono">
+        <p className="text-rblack text-3xl font-mono" id="abtmetext">
           Hi, I’m Issac Lin, a passionate web developer who loves turning ideas
           into interactive and engaging web experiences. I specialize in
           TypeScript, JavaScript, React, and Tailwind CSS, focusing on creating
